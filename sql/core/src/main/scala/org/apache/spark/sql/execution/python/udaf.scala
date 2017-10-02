f3f89a9b448b3cd92307bf664f126491e93875e8
Takuya UESHIN <ueshin@databricks.com>
2017-10-02 16:36:27 +0900

    Add UserDefinedAggregatePythonFunction.

--- sql/core/src/main/scala/org/apache/spark/sql/execution/python/udaf.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/python/udaf.scala
@@ -17,13 +17,77 @@
 
 package org.apache.spark.sql.execution.python
 
+import org.apache.spark.api.python.PythonFunction
 import org.apache.spark.internal.Logging
+import org.apache.spark.sql.Column
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.expressions._
 import org.apache.spark.sql.catalyst.expressions.aggregate._
 import org.apache.spark.sql.catalyst.expressions.codegen.GenerateMutableProjection
 import org.apache.spark.sql.catalyst.util.{ArrayData, ArrayDataBuffer}
 import org.apache.spark.sql.types._
+
+/**
+ * A user-defined aggregate Python function. This is used by the Python API.
+ */
+case class UserDefinedAggregatePythonFunction(
+    name: String,
+    evaluate: PythonFunction,
+    returnType: DataType,
+    partial: PythonFunction,
+    bufferType: DataType) {
+
+  /**
+   * Creates a `Column` for this UDAF using given `Column`s as input arguments.
+   */
+  @scala.annotation.varargs
+  def apply(exprs: Column*): Column = {
+    val aggregateExpression =
+      AggregateExpression(
+        PythonUDAF(exprs.map(_.expr), this),
+        Complete,
+        isDistinct = false)
+    Column(aggregateExpression)
+  }
+}
+
+/**
+ * The internal wrapper used to hook a [[UserDefinedAggregatePythonFunction]] `udaf` in the
+ * internal aggregation code path.
+ */
+case class PythonUDAF(
+    children: Seq[Expression],
+    udaf: UserDefinedAggregatePythonFunction)
+  extends AggregateFunction
+  with Unevaluable
+  with NonSQLExpression
+  with UserDefinedExpression {
+
+  override def nullable: Boolean = true
+
+  override def dataType: DataType = udaf.returnType
+
+  override val aggBufferSchema: StructType = udaf.bufferType match {
+    case StructType(fields) => StructType(fields.map { field =>
+      StructField(s"${udaf.name}.${field.name}",
+        ArrayType(field.dataType, containsNull = field.nullable), nullable = false)
+    })
+    case dt => new StructType().add(udaf.name, ArrayType(dt, containsNull = true), nullable = false)
+  }
+
+  override val aggBufferAttributes: Seq[AttributeReference] = aggBufferSchema.toAttributes
+
+  // Note: although this simply copies aggBufferAttributes, this common code can not be placed
+  // in the superclass because that will lead to initialization ordering issues.
+  override val inputAggBufferAttributes: Seq[AttributeReference] =
+    aggBufferAttributes.map(_.newInstance())
+
+  override def toString: String = {
+    s"${udaf.name}(${children.mkString(",")})"
+  }
+
+  override def nodeName: String = udaf.name
+}
 
 case class BufferInputs(
     children: Seq[Expression],
